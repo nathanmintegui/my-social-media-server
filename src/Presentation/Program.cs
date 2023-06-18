c04749136bc8a6b675ba0b4d0df38453d260625d
nathanmintegui <nathanberger.rs@hotmail.com>
2023-06-18 18:41:32 -0300

    feat(swagger): format swagger date input to yyyy--mm-dd

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -2,6 +2,8 @@ using Application.Contracts.Documents;
 using Application.Implementations.Services;
 using Domain.Contracts.Repositories;
 using Infrastructure.Repositories;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
 using Presentation.Security;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.MapType<DateTime>(() => new OpenApiSchema
+    {
+        Type = "string",
+        Format = "date",
+        Example = new OpenApiString("yyyy-mm-dd")
+    });
+    
+});
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IAuthService, AuthService>();
