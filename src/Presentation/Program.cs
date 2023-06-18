becd4ae111406c1c9bf2bb2826fad848dab241b3
nathanmintegui <nathanberger.rs@hotmail.com>
2023-06-18 18:43:08 -0300

    feat(swagger): add Bearer token save option

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -19,7 +19,30 @@ builder.Services.AddSwaggerGen(option =>
         Format = "date",
         Example = new OpenApiString("yyyy-mm-dd")
     });
-    
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "My Authorize API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
 });
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
