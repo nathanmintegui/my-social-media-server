33fb9ac2ff72b5e5ff7734af1f4a485bf7a874df
nathanmintegui <nathanberger.rs@hotmail.com>
2023-06-18 17:15:47 -0300

    feat(service): create sign up service method

--- src/Application/Contracts/Documents/IAuthService.cs
+++ src/Application/Contracts/Documents/IAuthService.cs
@@ -1,9 +1,11 @@
-using Application.Contracts.Documents.Requests;
+using Application.Contracts.Documents.Requests.Auth;
 using Application.Contracts.Documents.Responses;
+using Domain.Models;
 
 namespace Application.Contracts.Documents;
 
 public interface IAuthService
 {
+    Task<UserResponse> SignUpAsync(SignUpRequest request);
     Task<LoginResponse> ValidateLoginAsync(LoginRequest request);
 }
