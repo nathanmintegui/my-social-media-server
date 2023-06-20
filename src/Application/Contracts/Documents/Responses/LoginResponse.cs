e9220e2e01d2c39ed1a0289fe25a7aa5fe401dba
nathanmintegui <nathanberger.rs@hotmail.com>
2023-06-19 23:14:06 -0300

    feat(response): extend Notifiable class inside login response

--- src/Application/Contracts/Documents/Responses/LoginResponse.cs
+++ src/Application/Contracts/Documents/Responses/LoginResponse.cs
@@ -1,11 +1,12 @@
+using Application.Implementations.Validations;
 using static System.String;
 
 namespace Application.Contracts.Documents.Responses;
 
-public class LoginResponse
+public class LoginResponse : Notifiable
 {
     public bool IsAuthenticated { get; set; } = false;
     public int Id { get; set; }
-    public string Username { get; set; } = Empty;
+    public string Name { get; set; } = Empty;
     public string Email { get; set; } = Empty;
 }
