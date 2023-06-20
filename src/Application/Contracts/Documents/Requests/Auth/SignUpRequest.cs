467cd0f84417771aae1065b074ab67d4c4b6c87f
nathanmintegui <nathanberger.rs@hotmail.com>
2023-06-19 21:43:04 -0300

    feat(request): add Data Annotations on sign up request

--- src/Application/Contracts/Documents/Requests/Auth/SignUpRequest.cs
+++ src/Application/Contracts/Documents/Requests/Auth/SignUpRequest.cs
@@ -1,14 +1,31 @@
+using System.ComponentModel.DataAnnotations;
 using static System.String;
 
 namespace Application.Contracts.Documents.Requests.Auth;
 
 public class SignUpRequest
 {
+    [Required(ErrorMessage = "O campo nome é obrigatório.")]
+    [MaxLength(255)]
     public string Name { get; set; } = Empty;
+
+    [Required(ErrorMessage = "O campo email é obrigatório.")]
+    [MaxLength(255)]
+    [EmailAddress(ErrorMessage = "Este campo deve ter um endereço de email válido.")]
     public string Email { get; set; } = Empty;
-    public string? Nickname { get; set; }
+
+    [MaxLength(255)] public string? Nickname { get; set; }
+
+    [Required(ErrorMessage = "O campo data de nascimento é obrigatório.")]
     public DateTime BirthDate { get; set; }
-    public string? Cep { get; set; }
+
+    [Required(ErrorMessage = "O campo CEP é obrigatório.")]
+    [MaxLength(8)]
+    public string Cep { get; set; } = Empty;
+
+    [Required(ErrorMessage = "O campo senha é obrigatório.")]
+    [MaxLength(255)]
     public string Password { get; set; } = Empty;
-    public string? Photo { get; set; }
+
+    [MaxLength(512)] public string? Photo { get; set; }
 }
