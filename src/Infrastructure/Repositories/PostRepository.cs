1fca450bb449ab4e12088cb52171d69bd51501e2
nathanmintegui <nathanberger.rs@hotmail.com>
2023-06-22 22:20:20 -0300

    feat(repository): implement create post on repository

---
+++ src/Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,39 @@
+using Dapper;
+using Domain.Contracts.Repositories;
+using Domain.Models;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Infrastructure.Repositories;
+
+public class PostRepository : IPostRepository
+{
+    private readonly NpgsqlConnection _connection;
+
+    public PostRepository(IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        _connection = new NpgsqlConnection(connectionString);
+        _connection.Open();
+    }
+
+    public async Task<Post?> CreatePostAsync(Post post, int ownerId)
+    {
+        const string query = @"INSERT INTO post (ownerid, image, message, privacy, created_at)
+        VALUES (@OwnerId, @Image, @Message, @Privacy, @CreatedAt) returning *";
+
+        var parameters = new
+        {
+            OwnerId = ownerId,
+            Image = post.Image,
+            Message = post.Message,
+            Privacy = post.Privacy,
+            CreatedAt = post.CreatedAt
+        };
+
+        var response = await _connection.QueryFirstOrDefaultAsync<Post>(query, parameters);
+
+        return response;
+    }
+}
