cce2d27349e6ca3e389213217ed82fb23402d85c
nathanmintegui <nathanberger.rs@hotmail.com>
2024-04-06 12:36:55 -0300

    chore(get-user-public-posts): remove key tag due MatchNamesWithUnderscore config to dapper

--- src/Domain/Models/Comment.cs
+++ src/Domain/Models/Comment.cs
@@ -1,11 +1,10 @@
-using System.ComponentModel.DataAnnotations;
 using static System.String;
 
 namespace Domain.Models;
 
 public class Comment
 {
-    [Key] public int CommentId { get; private set; }
+    public int CommentId { get; private set; }
     public string Message { get; private set; } = Empty;
     public DateTime CreatedAt { get; private set; }
     public int PostId { get; private set; }
